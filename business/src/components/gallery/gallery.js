2451e46d0863b5136f7fbf151510b0ca349f63c0
thinker3197 <ashishgupta.3197@gmail.com>
2018-04-04 14:49:04 +0530

    Fix responsive issues

--- business/src/components/gallery/gallery.js
+++ business/src/components/gallery/gallery.js
@@ -8,13 +8,13 @@ export default class Gallery extends Component {
 	render() {
 		return (
       <div class={style.gallery}>
-        <div class={style.logoWrapper}>
-          <img src={logo} class={style.logo} alt="" />
-          <p class={style.logoText}>
-            <span class={style.flag}>🇮🇳 </span>Made in India
-          </p>
-        </div>
         <div class={cx(style.background, style.overlay)}>
+          <div class={style.logoWrapper}>
+            <img src={logo} class={style.logo} alt="" />
+            <p class={style.logoText}>
+              <span class={style.flag}>🇮🇳 </span>Made in India
+            </p>
+          </div>
           <p class={style.description}>“Jobstar has been a great resource to help us meet spikes in our demand during Maha bachat sales - a previously unavailable solution that has allowed us a lot of flexibility in managing the headcount in our stores.”
             <br />
             <span class={style.title}>Zone Head - Future Group</span>
