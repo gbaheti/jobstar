20a6eea99ea05f0a0facdae2ac4f109ba0b8f4c8
thinker3197 <ashishgupta.3197@gmail.com>
2018-04-04 17:13:05 +0530

    add build to master

---
+++ business/ssr-build/3.chunk.9ce2b.js
@@ -0,0 +1,13 @@
+exports.ids = [3];
+exports.modules = {
+
+/***/ "BtRA":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+module.exports = {"wrapper":"wrapper__yQ96r","title":"title__2WSDM","list":"list__3a7c1","listItem":"listItem__RFJur"};
+
+/***/ })
+
+};;
+//# sourceMappingURL=3.chunk.9ce2b.js.map
---
+++ business/ssr-build/5.chunk.8967f.js
@@ -0,0 +1,13 @@
+exports.ids = [5];
+exports.modules = {
+
+/***/ "rTUg":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+module.exports = {"wrapper":"wrapper__3rlR5","title":"title__1Bc3k","check":"check__1Uxx_"};
+
+/***/ })
+
+};;
+//# sourceMappingURL=5.chunk.8967f.js.map
