fc2b6d1e9941c9faea733d1fb8fe4a170de6a9e0
thinker3197 <ashishgupta.3197@gmail.com>
2018-04-07 16:11:57 +0530

    remove build from master

--- business/build/3.chunk.2496b.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{BtRA:function(t){t.exports={wrapper:"wrapper__yQ96r",title:"title__2WSDM",list:"list__3a7c1",listItem:"listItem__RFJur"}}});
-//# sourceMappingURL=3.chunk.2496b.js.map
