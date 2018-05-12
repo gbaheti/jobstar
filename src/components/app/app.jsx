a658987d8ac7eb5c8a7430a778d2ef94ab238b2f
thinker3197 <ashishgupta.3197@gmail.com>
2018-05-12 08:02:42 +0530

    Add ga

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+import ReactGA from "react-ga";
 
 import Home from "../home";
 import Business from "../business";
 import PrivacyPolicy from "../privacyPolicy";
 
+ReactGA.initialize("UA-33385057-4");
+
+const history = createHistory();
+const historyListener = (location) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+};
+
+history.listen(historyListener);
+
 const App = () => {
+  // Fire history listener on page load
+  historyListener(window.location);
+
   return (
-    <Router>
+    <Router history={history}>
       <Switch>
         <Route exact path="/business" component={Business} />
         <Route exact path="/privacy-policy" component={PrivacyPolicy} />
