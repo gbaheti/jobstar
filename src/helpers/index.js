477a79fcfc14650af1a19a5a3dd3a0ede8688baa
thinker3197 <ashishgupta.3197@gmail.com>
2018-05-14 12:50:01 +0530

    Fix job api response feilds

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,12 @@
 import moment from "moment";
-import { replaceAll, titleize, humanize, capitalize, truncate, dasherize } from "underscore.string";
+import {
+  replaceAll,
+  titleize,
+  humanize,
+  capitalize,
+  truncate,
+  dasherize
+} from "underscore.string";
 
 export const generateShareableJobUrl = (id, category, location) => {
   const hashText = id + category + location;
@@ -12,43 +19,29 @@ export const normalizeJob = job => {
   const template = job.job_template;
   const details = template.other_details;
 
-  if (
-    !details ||
-    !details.Role ||
-    !details.Income ||
-    !template.tags ||
-    !details.Timings ||
-    !details.location ||
-    !details.Channel ||
-    !details["Age Limit"] ||
-    !details["Languages known"] ||
-    !details["Job Benefits"] ||
-    !details.Skills ||
-    !template.category ||
-    !Array.isArray(details.Skills)
-  )
+  if (!template || !details || !details.income || !Array.isArray(details.skills))
     return null;
 
   const data = {
     ids: job.ids || job.id,
     type: template.category,
     applied: job.applied || false,
-    category: details.Role,
-    salary: details.Income.salary,
-    salaryType: details.Income.type,
+    category: details.role,
+    salary: details.income.salary,
+    salaryType: details.income.type,
     employer: template.tags[0],
-    timings: details.Timings,
+    timings: details.timings,
     location: details.location,
-    channel: details.Channel,
+    channel: details.channel,
     applicants: Math.floor(Math.random() * 10),
-    ageLimit: !(details["Age Limit"] === "Not required"),
+    ageLimit: !(details.age_limit === "Not required"),
     minAge: 16,
     maxAge: 60,
-    identity: details["Identity Proof"],
-    languages: details["Languages known"],
-    benefits: details["Job Benefits"],
-    skills: details.Skills,
-    description: template.description,
+    identity: details.identity_proof,
+    languages: details.languages_known,
+    benefits: details.job_benefits,
+    skills: details.skills,
+    description: template.description
   };
 
   data.category = titleize(humanize(data.category));
@@ -61,15 +54,21 @@ export const normalizeJob = job => {
 };
 
 export const normalizeProfile = (profile, placeholder = "-") => {
-  const dob = profile.dob ? moment(profile.dob).format("DD/MM/YYYY") : "//";
-  const name = profile.last_name ? profile.first_name + " " + profile.last_name : profile.first_name;
+  const dob = profile.dob
+    ? moment(profile.dob).format("DD/MM/YYYY")
+    : "//";
+  const name = profile.last_name
+    ? profile.first_name + " " + profile.last_name
+    : profile.first_name;
 
   return {
     avatar: profile.avatar || placeholder,
     name: name || placeholder,
     phone: profile.phone || placeholder,
     city: profile.city || placeholder,
-    dob: dob !== "//" ? dob : placeholder,
+    dob: dob !== "//"
+      ? dob
+      : placeholder,
     dobDate: dob.split("/")[0] || placeholder,
     dobMonth: dob.split("/")[1] || placeholder,
     dobYear: dob.split("/")[2] || placeholder,
@@ -77,6 +76,6 @@ export const normalizeProfile = (profile, placeholder = "-") => {
     lookingFor: profile.lookingFor || placeholder,
     gender: profile.gender || placeholder,
     aadhaar: profile.aadhaar || placeholder,
-    bio: profile.bio || placeholder,
+    bio: profile.bio || placeholder
   };
 };
